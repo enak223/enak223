93732dad2ec30611687a9aa2e0b7c369575db673
Eliezer <53668366+enak223@users.noreply.github.com>
2025-08-25 17:49:50 -0400

    Create WN10-AC-000035.ps1

---
+++ STIGS/WN10-AC-000035.ps1
@@ -0,0 +1,44 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Eliezer Fuentes
+    LinkedIn        : linkedin.com/in/eliezerfuentes/
+    GitHub          : github.com/enak223/
+    Date Created    : 2025-08-24
+    Last Modified   : 2025-08-24
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AC-000035
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+# Ensure script runs with Administrator privileges
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltinRole] "Administrator")) {
+    Write-Error "This script must be run as Administrator."
+    exit
+}
+
+# Desired minimum password length (STIG requires at least 14)
+$DesiredMinPasswordLength = 14
+
+Write-Output "Current password policy settings:"
+net accounts
+
+Write-Output "`nConfiguring Minimum Password Length to $DesiredMinPasswordLength characters..."
+net accounts /minpwlen:$DesiredMinPasswordLength
+
+Write-Output "`nUpdated password policy settings:"
+net accounts
