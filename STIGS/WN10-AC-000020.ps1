58790543325c423e5cdffb96a96d4461e1daca4f
Eliezer <53668366+enak223@users.noreply.github.com>
2025-08-27 08:22:34 -0400

    WN10-AC-000020.ps1

--- STIGS/WN10-AC-000020.ps1
+++ STIGS/WN10-AC-000020.ps1
@@ -22,7 +22,7 @@
 .USAGE
     Put any usage instructions here.
     Example syntax:
-    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+    PS C:\> .\WN10-AC-000020.ps1
 #>
 
 # Ensure script runs as admin
